17a625975870d56a511f17b7d272f0e37f7065cd
nakul9423 <nakulwaghmare9423@gmail.com>
2026-04-29 09:09:39 +0530

    UseState and useEffect Hooks

---
+++ React Js/UI-329/src/Constant/mockData.js
@@ -0,0 +1,50 @@
+ export const courseDetails = [
+        {
+            "name": "Full Stack",
+            "image": "https://via.placeholder.com/150",
+            "buttonName": "View Details"
+        },
+        {
+            "name": "MEAN Stack",
+            "image": "https://via.placeholder.com/150",
+            "buttonName": "View Details"
+        },
+        {
+            "name": "MERN Stack",
+            "image": "https://via.placeholder.com/150",
+            "buttonName": "View Details"
+        },
+        {
+            "name": "Agentic AI",
+            "image": "https://via.placeholder.com/150",
+            "buttonName": "View Details"
+        },
+
+    ]
+
+
+ export const studDetails = [
+        {
+            "name": "John Dev",
+            "email": "johndev@gmail.com",
+            "Mobile": 9767171696
+        },
+        {
+            "name": "John Dev",
+            "email": "johndev@gmail.com",
+            "Mobile": 9767171696
+        },
+        {
+           "name": "John Dev",
+            "email": "johndev@gmail.com",
+            "Mobile": 9767171696
+        },
+        {
+            "name": "John Dev",
+            "email": "johndev@gmail.com",
+            "Mobile": 9767171696
+        },
+
+    ]
+
+
