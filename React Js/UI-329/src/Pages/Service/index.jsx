483672b72ecb5df8848f98f9488cc34ae3f0e593
nakul9423 <nakulwaghmare9423@gmail.com>
2026-05-10 09:33:57 +0530

    API intigration with fetch methods

--- React Js/UI-329/src/Pages/Service/index.jsx
+++ React Js/UI-329/src/Pages/Service/index.jsx
@@ -1,11 +1,49 @@
 
+import { useEffect, useState } from 'react'
 import './Service.css'
 
-const Service=()=>{
+const Service = () => {
+    const [serviceDetails, setServiceDetails] = useState();
+    console.log(serviceDetails, 'serviceDetails@@###')
+    useEffect(() => {
+        getServiceProductDetails();
+    }, []);
+
+    const getServiceProductDetails = () => {
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setServiceDetails(json)
+            )
+
+    }
+
+    const redirect=(id)=>{
+        console.log(id,'item.id@@####');
+        localStorage.setItem('id',JSON.stringify(id));
+        
+    }
 
     return (
         <div className="serviceMainWrap">
-            <h3> THis is the Service Components</h3>
+            <h3> This are the services </h3>
+            <div className='row mt-4'>
+                {serviceDetails.slice(0,4).map((item, index) => {
+                    return <div className='col-md-3'>
+                        <div className='card' key={index}>
+                            <div className='card-header text-center'>Title:{item.title}</div>
+                            <div className='card-body text-center'>
+                                <p>User ID:{item.userId}</p>
+                                <p>Id:{item.id} </p>
+                                <p>Description:{item.body}</p>
+                            </div>
+                            <div className='card-footer text-center'><button className='btn btn-primary' onClick={()=>{redirect(item.id)}}>View Details</button></div>
+                        </div>
+                    </div>
+                })}
+            </div>
+
+
         </div>
     )
 
